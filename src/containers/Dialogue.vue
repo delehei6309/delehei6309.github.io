e3b7ae9068c08d11fe9fc95f9fbec665abeea827
delehei6309 <wangchunting7@163.com>
2019-03-03 12:06:07 +0800

    dev

--- src/containers/Dialogue.vue
+++ src/containers/Dialogue.vue
@@ -36,6 +36,23 @@
                 data:[
                     '嗨！👋','大夢誰先覺🙌','平生我自知','草堂春睡足','窗外日遲遲','一尊搔首东窗里。','想渊明、停云诗就，此时风味。','江左沈酣求名者，岂识浊醪妙理。','回首叫、云飞风起。','不恨古人吾不见，恨古人、不见吾狂耳。','知我者，二三子','人生若只如初见，','何事秋风悲画扇。','等闲变却故人心，','却道故人心易变。','骊山语罢清宵半，','泪雨霖铃终不怨。','何如薄幸锦衣郎，','比翼连枝当日愿'
                 ],
+                data1:[
+                    {
+                        q:'江左沈酣求名者，岂识浊醪妙理。',
+                        a:[
+                            {
+                               value:'A',
+                               label:"比翼连枝当日愿" 
+                            },{
+                               value:'B',
+                               label:"骊山语罢清宵半，" 
+                            },{
+                               value:'C',
+                               label:"何事秋风悲画扇。" 
+                            }
+                        ]
+                    }
+                ],
                 items:[]
             }
         },
@@ -76,6 +93,7 @@
                     let top = document.scrollingElement.scrollTop;
                     if(top < height){
                         top++;
+
                         document.scrollingElement.scrollTop = top;
                         timer = requestAnimationFrame(fun);
                     }else{
